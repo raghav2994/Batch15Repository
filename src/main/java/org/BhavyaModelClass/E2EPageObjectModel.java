9a620d2a89fa05bff6bcccd73499983b21f5343a
Bhavya <bhavyathapar15@gmail.com>
2025-03-01 12:46:31 +0530

    Bhavya Code Enhancement

--- src/main/java/org/BhavyaModelClass/E2EPageObjectModel.java
+++ src/main/java/org/BhavyaModelClass/E2EPageObjectModel.java
@@ -1,22 +1,50 @@
 package org.BhavyaModelClass;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 public class E2EPageObjectModel {
     WebDriver driver;
     Actions action;
-    private String ProductPageUrl="https://demo.evershop.io/men/nike-react-phantom-run-flyknit-2-180";
+    private String ProductPageUrl="https://demo.evershop.io/men";
+    private By ProductLink=By.xpath("//div/a/span[text()='Nike react phantom run flyknit 2']");
     private By size=By.partialLinkText("X");
     private By colour=By.partialLinkText("Black");
     private By quantity=By.xpath("//div/input[@type='text'][@name='qty']");
-    private By submitButton=By.xpath("//div/button[@type='button']/span[text()='ADD TO CART']");
+    private By addToCart=By.xpath("//div/button[@type='button']/span[text()='ADD TO CART']");
     private By alertBox=By.xpath("//div[@role='alert']");
 
+
+    private By userNameField = By.name("email");
+    private By passwordField = By.name("password");
+    private By sumbmitButtonXpath = By.xpath("//button[@type='submit']");
+    private String homePageURL = "https://demo.evershop.io/account/login";
+
+    private By viewCart=By.partialLinkText("VIEW CART");
+    private By checkout=By.xpath("//div/a/span[text()='CHECKOUT']");
+    private By contactDetails =By.xpath("//div[@class='col-span-2']/span");
+
+    public E2EPageObjectModel hitTheUrl(){
+        driver.get(homePageURL);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        return this;
+    }
+
+    public E2EPageObjectModel fillCredentials(String userName, String password){
+        driver.findElement(userNameField).sendKeys(userName);
+        driver.findElement(passwordField).sendKeys(password);
+        return this;
+    }
+    public void clickOnSubmitButton(){
+        driver.findElement(sumbmitButtonXpath).click();
+    }
 
     public E2EPageObjectModel(WebDriver driver){
         this.driver = driver;
@@ -25,17 +53,20 @@ public class E2EPageObjectModel {
     public void hittingProductPageUrl(){
         driver.get(ProductPageUrl);
     }
+    public void selectProduct(){
+        driver.findElement(ProductLink).click();
+    }
     public WebElement selectSize(){
         return driver.findElement(size);
     }
     public WebElement selectColour(){
         return driver.findElement(colour);
     }
-    public void selectQuantity(){
-        driver.findElement(quantity).sendKeys("1000000");
+    public void selectQuantity(String qty){
+        driver.findElement(quantity).sendKeys(qty);
     }
-    public void clickingSubmitButton(){
-        driver.findElement(submitButton).click();
+    public void clickingAddToCart(){
+        driver.findElement(addToCart).click();
     }
     public boolean isSizeSelected(){
         return driver.findElement(size).isEnabled();
@@ -50,5 +81,14 @@ public class E2EPageObjectModel {
         }
 
     }
+    public void viewCart(){
+        driver.findElement(viewCart).click();
+    }
+    public void selectCheckoutButton(){
+        driver.findElement(checkout).click();
+    }
+    public String verifyContactDetails(){
+        return driver.findElement(contactDetails).getText();
 
+    }
 }
--- src/test/java/org/Bhavya/E2EJourney.java
+++ src/test/java/org/Bhavya/E2EJourney.java
@@ -5,13 +5,16 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
 public class E2EJourney {
     WebDriver driver;
@@ -19,9 +22,11 @@ public class E2EJourney {
     E2EPageObjectModel pom;
     WebDriverWait wait;
     Actions action;
+    String email="akhiljda@gmail.com";
+    String password="Password";
 
-    @BeforeMethod
-    public void beforeMethod() throws MalformedURLException {
+    @BeforeClass
+    public void beforeClass() throws MalformedURLException {
         options = new ChromeOptions();
         driver = new RemoteWebDriver(new URL("http://192.168.56.1:4444"), options);
         pom = new E2EPageObjectModel(driver);
@@ -30,20 +35,32 @@ public class E2EJourney {
         action = new Actions(driver);
 
     }
+    @Test (priority = 0)
+    public void loginTest(){
+        pom.hitTheUrl();
+        pom.fillCredentials(email,password);
+        pom.clickOnSubmitButton();
 
-    @Test
+    }
+
+    @Test(priority = 1)
     public void addToCart() throws InterruptedException {
         pom.hittingProductPageUrl();
+        wait.until(ExpectedConditions.titleContains("An Amazing EverShop Store"));
+        pom.selectProduct();
+        Assert.assertEquals(driver.getTitle(),"Nike React Phantom Run Flyknit 2");
         pom.selectSize().click();
-        Assert.assertTrue(pom.isSizeSelected());
-//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(50));
-        Thread.sleep(5000);
+        Thread.sleep(1000);
         pom.selectColour().click();
-        Thread.sleep(5000);
-        pom.selectQuantity();
-        Thread.sleep(5000);
-        pom.clickingSubmitButton();
-        Thread.sleep(5000);
-        //Assert.assertTrue(pom.isAddedToCart());
+        Thread.sleep(1000);
+        pom.selectQuantity("1");
+        Thread.sleep(1000);
+        pom.clickingAddToCart();
+        pom.viewCart();
+        pom.selectCheckoutButton();
+        Assert.assertEquals(pom.verifyContactDetails(),email);
+
     }
+
+
 }
